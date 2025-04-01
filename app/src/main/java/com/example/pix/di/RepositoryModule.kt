1a6534f762d68d8734b4ab2eb6cc28a6d527ee95
Semyon <xuevbli@gmail.com>
2025-04-02 00:21:31 +1200

    feat: Create RepositoryModule

---
+++ app/src/main/java/com/example/pix/di/RepositoryModule.kt
@@ -0,0 +1,19 @@
+package com.example.pix.di
+
+import com.example.pix.domain.repository.PictureRepository
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class RepositoryModule {
+
+    @Binds
+    @Singleton
+    abstract fun bindPictureRepository(
+        pictureRepositoryImpl: PictureRepositoryImpl
+    ): PictureRepository
+}
