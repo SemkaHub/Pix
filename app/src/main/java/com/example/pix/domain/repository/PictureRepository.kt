3b7d7e7e3f76e34fa9ffbe1195392b5cab50b40e
Semyon <xuevbli@gmail.com>
2025-04-02 00:04:17 +1200

    refactor: Rename getPicture to getPictureById in PictureRepository

--- app/src/main/java/com/example/pix/domain/repository/PictureRepository.kt
+++ app/src/main/java/com/example/pix/domain/repository/PictureRepository.kt
@@ -4,5 +4,5 @@ import com.example.pix.domain.model.Picture
 
 interface PictureRepository {
     suspend fun getPictures(): Result<List<Picture>>
-    suspend fun getPicture(id: String): Result<Picture>
+    suspend fun getPictureById(id: String): Result<Picture>
 }
