070cdb67eb03c3ee65acf84d0d6b5c264960fc72
Semyon <xuevbli@gmail.com>
2025-04-01 15:18:17 +1200

    chore: Update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
     defaultConfig {
         applicationId = "com.example.pix"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
