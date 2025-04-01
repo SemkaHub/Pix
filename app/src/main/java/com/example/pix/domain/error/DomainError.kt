c101896c2de16cef1e03775fcc848a30430a2995
Semyon <xuevbli@gmail.com>
2025-04-01 22:53:08 +1200

    feat: Create DomainError sealed class

---
+++ app/src/main/java/com/example/pix/domain/error/DomainError.kt
@@ -0,0 +1,27 @@
+package com.example.pix.domain.error
+
+sealed class DomainError {
+    data object Network : DomainError() {
+        private fun readResolve(): Any = Network
+    }
+
+    data object Database : DomainError() {
+        private fun readResolve(): Any = Database
+    }
+
+    sealed class Server : DomainError() {
+        // 400
+        data object BadRequest : Server() {
+            private fun readResolve(): Any = BadRequest
+        }
+
+        // 404
+        data object NotFound : Server() {
+            private fun readResolve(): Any = NotFound
+        }
+
+        data class Generic(val code: Int, val errorMessage: String?) : Server()
+    }
+
+    data class Unknown(val error: Throwable) : DomainError()
+}
