db26d95c79a55829e66c2a34f18958800187d01c
Semyon <xuevbli@gmail.com>
2025-04-02 00:34:40 +1200

    refactor: Move PictureDao to dao package

--- app/src/main/java/com/example/pix/data/room/dao/PictureDao.kt
+++ app/src/main/java/com/example/pix/data/room/dao/PictureDao.kt
@@ -1,19 +1,19 @@
-package com.example.pix.data.room
+package com.example.pix.data.room.dao
 
 import androidx.room.Dao
-import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import com.example.pix.data.room.entity.PictureEntity
 
 @Dao
 interface PictureDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insertAll(users: List<PictureDbo>)
+    suspend fun insertAll(users: List<PictureEntity>)
 
     @Query("delete from pictures")
     suspend fun clearAll()
 
     @Query("select * from pictures")
-    suspend fun getAll(): List<PictureDbo>
+    suspend fun getAll(): List<PictureEntity>
 }
