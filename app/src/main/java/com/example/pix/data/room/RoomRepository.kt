4cc3abdcda51be6cbc049b393ad77e3a2cb09beb
Semyon <xuevbli@gmail.com>
2025-04-02 16:44:44 +1200

    feat: Create RoomRepository

---
+++ app/src/main/java/com/example/pix/data/room/RoomRepository.kt
@@ -0,0 +1,26 @@
+package com.example.pix.data.room
+
+import com.example.pix.data.room.dao.PictureDao
+import com.example.pix.data.room.mapper.toDomain
+import com.example.pix.data.room.mapper.toEntity
+import com.example.pix.domain.model.Picture
+import javax.inject.Inject
+
+class RoomRepository @Inject constructor(
+    private val pictureDao: PictureDao
+){
+    suspend fun getPictures(): List<Picture> {
+        val entities = pictureDao.getAll()
+        return entities.map { it.toDomain() }
+    }
+
+    suspend fun clearAll() {
+        pictureDao.clearAll()
+    }
+
+    suspend fun insertAll(pictures: List<Picture>) {
+        val entities = pictures.map { it.toEntity() }
+        pictureDao.insertAll(entities)
+    }
+
+}
