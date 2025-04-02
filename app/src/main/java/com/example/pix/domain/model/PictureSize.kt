91545e217b0cbca54f0db83ed1529e50ac00664a
Semyon <xuevbli@gmail.com>
2025-04-02 16:33:14 +1200

    feat: Create PictureSize enum

---
+++ app/src/main/java/com/example/pix/domain/model/PictureSize.kt
@@ -0,0 +1,18 @@
+package com.example.pix.domain.model
+
+enum class PictureSize(
+    val prefix: String,
+    val width: Int
+) {
+    Q("q", 100),
+    M("m", 240),
+    N("n", 320),
+    W("w", 400),
+    Z("z", 640),
+    C("c", 800),
+    B("b", 1024),
+    H("h", 1600),
+    K("k", 2048),
+    K3("3k", 3072),
+    K4("4k", 4096);
+}
