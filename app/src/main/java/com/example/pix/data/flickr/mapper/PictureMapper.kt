b6cdfcd89f4d47a512f2331c9bf4a0382e910129
Semyon <xuevbli@gmail.com>
2025-04-02 16:32:45 +1200

    feat: Add id to PhotoDto.toDomain mapping

--- app/src/main/java/com/example/pix/data/flickr/mapper/PictureMapper.kt
+++ app/src/main/java/com/example/pix/data/flickr/mapper/PictureMapper.kt
@@ -4,7 +4,8 @@ import com.example.pix.data.flickr.dto.PhotoDto
 import com.example.pix.domain.model.Picture
 
 // https://www.flickr.com/services/api/misc.urls.html
-fun PhotoDto.toEntity(quality: String): Picture = Picture(
+fun PhotoDto.toDomain(quality: String): Picture = Picture(
+    id = id,
     title = title,
     url = "https://live.staticflickr.com/${server}/${id}_${secret}_${quality}.jpg",
 )
