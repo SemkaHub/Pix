de61c40b25ae2b3f46d31a14df850e791ed680b2
Semyon <xuevbli@gmail.com>
2025-04-02 17:28:14 +1200

    feat: Implement PictureRepository

---
+++ app/src/main/java/com/example/pix/data/repository/PictureRepositoryImpl.kt
@@ -0,0 +1,52 @@
+package com.example.pix.data.repository
+
+import com.example.pix.data.flickr.FlickrRepository
+import com.example.pix.data.room.RoomRepository
+import com.example.pix.domain.model.Picture
+import com.example.pix.domain.model.PictureSize
+import com.example.pix.domain.repository.PictureRepository
+import javax.inject.Inject
+
+class PictureRepositoryImpl @Inject constructor(
+    private val flickrRepository: FlickrRepository,
+    private val roomRepository: RoomRepository
+) : PictureRepository {
+
+    override suspend fun getPictures(): Result<List<Picture>> = runCatching {
+        val remotePictures = flickrRepository.search().getOrThrow()
+        roomRepository.clearAll()
+        roomRepository.insertAll(remotePictures)
+        roomRepository.getPictures()
+    }.fold(
+        onSuccess = { Result.success(it) },
+        onFailure = { Result.failure(it) }
+    )
+
+    // https://live.staticflickr.com/7372/12502775644_acfd415fa7_w.jpg
+    override suspend fun getPictureById(id: String): Result<Picture> = runCatching {
+        val picture = roomRepository.getById(id)
+        val betterQualityPicture = PictureSize.K.prefix
+        val oldUrl = picture.url
+        val newUrl = getBestQualityPictureUrl(oldUrl, betterQualityPicture)
+        picture.copy(url = newUrl)
+    }.fold(
+        onSuccess = { Result.success(it) },
+        onFailure = { Result.failure(it) }
+    )
+
+    private fun getBestQualityPictureUrl(oldUrl: String, quality: String): String {
+
+        // Находим индекс последнего символа '_'
+        val lastUnderscoreIndex = oldUrl.lastIndexOf('_')
+        // Находим индекс последнего символа '.'
+        val lastDotIndex = oldUrl.lastIndexOf('.')
+
+        // Часть строки до качества фото
+        val prefix = oldUrl.substring(0, lastUnderscoreIndex + 1)
+
+        // Часть строки после качества фото
+        val extension = oldUrl.substring(lastDotIndex)
+
+        return "$prefix$quality$extension"
+    }
+}
