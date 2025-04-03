bcc5041d73f0e566e072d8f3886a9c3ad75dc5d6
Semyon <xuevbli@gmail.com>
2025-04-04 00:16:22 +1200

    feat: Add PictureDetailScreenViewModel

---
+++ app/src/main/java/com/example/pix/ui/screen/picture/PictureDetailScreenViewModel.kt
@@ -0,0 +1,51 @@
+package com.example.pix.ui.screen.picture
+
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.pix.domain.error.DomainError
+import com.example.pix.domain.usecase.GetPictureByIdUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+private const val ERROR_PICTURE_ID_NULL = "PictureId is null"
+
+@HiltViewModel
+class PictureDetailScreenViewModel @Inject constructor(
+    private val getPictureByIdUseCase: GetPictureByIdUseCase,
+    savedStateHandle: SavedStateHandle
+) : ViewModel() {
+
+    private val _state = MutableStateFlow<PictureDetailUiState>(PictureDetailUiState.Loading)
+    val state = _state.asStateFlow()
+
+    private val pictureId: String? = savedStateHandle["pictureId"]
+
+    init {
+        loadPicture()
+    }
+
+    fun loadPicture() {
+
+        if (pictureId == null) {
+            _state.value =
+                PictureDetailUiState.Error(DomainError.Unknown(Throwable(ERROR_PICTURE_ID_NULL)))
+            return
+        }
+
+        _state.value = PictureDetailUiState.Loading
+        viewModelScope.launch {
+            getPictureByIdUseCase(pictureId)
+                .onSuccess { picture ->
+                    _state.value = PictureDetailUiState.Success(picture)
+                }
+                .onFailure { throwable ->
+                    val domainError = throwable as? DomainError ?: DomainError.Unknown(throwable)
+                    _state.value = PictureDetailUiState.Error(domainError)
+                }
+        }
+    }
+}
